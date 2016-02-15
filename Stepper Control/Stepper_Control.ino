1f4f23b145c569a4a2c7124872d2690139ea85c0
Mike Monson <Mike.03@me.com>
2016-02-15 15:49:33 -0800

    ME_106_Stepper_Motor Control
    Testing stepper motor control with xbox controller, arduino, CNC shield
    and motor drivers

--- Stepper Control/Stepper_Control.ino
+++ Stepper Control/Stepper_Control.ino
@@ -35,7 +35,7 @@
 ///
 /// @see		ReadMe.txt for references
 /// @n
-///
+/// For NEMA 17 200 pulses makes one full cycle
 
 
 // Core library for code-sense - IDE-based
@@ -73,7 +73,7 @@
 #endif // end IDE
 
 // Include application, user and local libraries
-
+#include <XBOXRECV.h>
 
 // Define structures and classes
 
@@ -81,17 +81,143 @@
 // Define variables and constants
 
 
+#define xDir 5
+#define yDir 6
+#define zDir 7
+
+#define xPulse 2
+#define yPulse  3
+#define zPulse 4
+
+
+#define stepperEnable = 8
+
+int speed = 600;
+int count = 0;
+bool FLAG = true;
+
+
+USB Usb;
+XBOXRECV Xbox(&Usb);
+
 // Prototypes
 
 
 // Add setup code
 void setup()
 {
+//initalize pins for stepper motors
+    for (int i = 2 ; i <= 8; i++) {
+        pinMode(i, OUTPUT);
+    }
+    
+//XBOX CONTROLLER SETUP
+    Serial.begin(115200);
+#if !defined(__MIPSEL__)
+    while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+    if (Usb.Init() == -1) {
+        Serial.print(F("\r\nOSC did not start"));
+        while (1); //halt
+    }
+    Serial.print(F("\r\nXbox Wireless Receiver Library Started"));
+    
+
 
 }
 
 // Add loop code
 void loop()
 {
+    Usb.Task();
+    
+    
+    if (FLAG && Xbox.Xbox360Connected[0]) {
+        Xbox.setAllOff();
+        delay(500);
+        Xbox.setLedOn(LED1);
+        delay(500);
+        Xbox.setLedOn(LED2);
+        delay(500);
+        Xbox.setLedOn(LED4);
+        delay(500);
+        Xbox.setLedOn(LED3);
+        delay(500);
+        Xbox.setLedOn(LED1);
+        
+        
+        FLAG = false;
+        Serial.print(F("\nController Connected!\n"));
+    }
+        
+        //decrease stepper speed
+        if (Xbox.getButtonPress(L)) {
+            speed += 1;
+            Serial.println("Speed Decreased...");
+        }
+        
+        //increase stepper speed
+        if (Xbox.getButtonPress(R)) {
+            speed -= 1;
+            Serial.println("Speed Increased...");
+        }
 
-}
+        //move barrel up
+        if (Xbox.getButtonPress(UP)) {
+            digitalWrite(yDir, HIGH);
+            digitalWrite(yPulse, HIGH);
+            delayMicroseconds(speed);
+            digitalWrite(yPulse, LOW);
+            Serial.println(" Up..");
+        }
+    
+    
+        //move barrel down
+        if (Xbox.getButtonPress(DOWN)) {
+            digitalWrite(yDir, LOW);
+            digitalWrite(yPulse, HIGH);
+            delayMicroseconds(speed);
+            digitalWrite(yPulse, LOW);
+            Serial.println(" Down...");
+
+            
+        }
+        //move barrel left
+        if (Xbox.getButtonPress(LEFT)) {
+            digitalWrite(xDir, HIGH);
+            digitalWrite(xPulse, HIGH);
+            delayMicroseconds(speed);
+            digitalWrite(xPulse, LOW);
+            Serial.println(" Left...");
+
+            
+        }
+        //move barrel right
+        if (Xbox.getButtonPress(RIGHT)) {
+            digitalWrite(xDir, LOW);
+            digitalWrite(xPulse, HIGH);
+            delayMicroseconds(speed);
+            digitalWrite(xPulse, LOW);
+            Serial.println(" Right...");
+
+            
+        }
+    
+    if (Xbox.getButtonClick(A)) {
+        digitalWrite(zPulse, HIGH);
+        delayMicroseconds(700);
+        digitalWrite(zPulse, LOW);
+        Serial.println("Toggle...");
+
+    }
+    
+    }
+
+    
+
+    
+   
+
+
+
+
